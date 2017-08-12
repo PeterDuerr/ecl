9955bec390d4b9bbcd3e9515329f5fa0fcb757d6
Peter Dürr <Peter.Duerr@sony.com>
2017-08-12 15:57:29 +0200

    Attempt no 3

--- build.sh
+++ build.sh
@@ -47,7 +47,7 @@ else
     # Build EKF shared library.
     mkdir Build -p
     cd Build
-    pip install --user ../EKF/tests/pytest/requirements.txt
+    pip install -r ../EKF/tests/pytest/requirements.txt
     cmake -DPythonTests=1 ../EKF
     make pytest
     cd ..
